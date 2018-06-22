f406478bb21d25f61226abff7eec6ab3bf0cb84f
מרדכי שישה <mordehaic@mrdkys-mac-mini.mynet>
2018-06-22 17:58:52 +0300

    fix merge that have to be

--- jet.js
+++ jet.js
@@ -3,9 +3,13 @@ class Jet {
     this.color = color;
     this.size = size;
   }
+
+  takeOff () {
+    console.log('lift off!');
+  }
   fly () {
     console.log('whoosh!');
-  }
+  };
 }
 
 module.exports = Jet;
